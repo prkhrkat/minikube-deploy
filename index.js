8b67f30ba206f9a222a5977f3ce36520bd3d5290
prkhrkat <prkhrkat@gmail.com>
2024-05-16 12:41:13 +0530

    commit

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Kubernetes!\n');
+const server = http.createServer(async (req, res) => {
+  try {
+    const response = await fetch(process.env.END_URL);
+    // log endurl to console
+    console.log('END_URL:', process.env.END_URL);
+    if (response.ok) {
+      const data = await response.text();
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(data);
+    } else {
+      throw new Error('Failed to fetch data');
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Error: ${error.message}`);
+  }
 });
 
 server.listen(port, hostname, () => {
